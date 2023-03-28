015fad8e94ca37f5fedd1ec58237848f963e248a
AlceMaia <alceniziamaia@gmail.com>
2023-03-27 23:36:21 -0300

    Alteração de imports
    Fiz alterações para conseguir utilizar useStates, BowserRout, Link e Route.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import logo from "../../assets/DncDetalhe.svg";
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import logo from '../../assets/DncDetalhe.svg';
 import './index.scss';
 
 export const Login = () => {
