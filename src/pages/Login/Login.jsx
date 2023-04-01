5aa639cda84c02b60d217382a02a78033bdc29ce
AlceMaia <alceniziamaia@gmail.com>
2023-04-01 18:58:17 -0300

    mensagem de erro ajustada. Faltando mensagem de confirmação

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,49 +1,46 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
 import logo from "../../assets/DncDetalhe.svg";
 import { Link } from 'react-router-dom';
 import './index.scss';
 
 
-=======
-import { BrowserRouter, Link, Route } from 'react-router-dom';
-import logo from '../../assets/DncDetalhe.svg';
-import './index.scss';
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Bem-vindo(a) à página inicial!</h1>
-      <Link to="/login">Voltar para a página de login</Link>
-    </div>
-  );
-};
->>>>>>> 350e1b9793a51bacdedbe87ab17e79fe76c6ecd2
 const Login = () => {
   const [cpf, setCpf] = useState('');
   const [password, setPassword] = useState('');
   const [cpfError, setCpfError] = useState('');
   const [passwordError, setPasswordError] = useState('');
+  const [isConfirmed, setIsConfirmed] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> 350e1b9793a51bacdedbe87ab17e79fe76c6ecd2
+    let cpfValidSubmit = true;
+    let passwordValidSubmit = true;
+
+
     if (cpf.trim() === '') {
       setCpfError('Por favor, insira seu CPF.');
+      cpfValidSubmit= false;
     } else if (!/^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(cpf)) {
       setCpfError('Por favor, insira um CPF válido.');
+      cpfValidSubmit = false;
     } else {
       setCpfError('');
     }
 
     if (password.trim() === '') {
       setPasswordError('Por favor, insira sua senha.');
+      passwordValidSubmit = false;
     } else {
       setPasswordError('');
+    }
+
+    if (cpfValidSubmit && passwordValidSubmit) {
+      //Simulação de chamada para um API de autenticação
+      setTimeout(() => {
+        setIsConfirmed(true);
+        console.log('Login realizado com sucesso!');
+      }, 2000);
     }
   };
 
@@ -54,7 +51,6 @@ const Login = () => {
           <img src={logo} alt="" />
         </nav>
         <form className="login__form" onSubmit={handleSubmit}>
-<<<<<<< HEAD
           <h1>Acesse com seu login ou cadastre-se!</h1>
           <h2>Você pode entrar com o seu CPF</h2>
           <div className="login__input-wrapper">
@@ -80,21 +76,9 @@ const Login = () => {
           </div>
           <button type="submit">Entrar</button>
         </form>
-=======
-              <h1>Acesse com seu login ou cadastre-se!</h1>
-              <h2>você pode entrar com o seu CPF</h2>
-              <div className="login__input-wrapper">
-                  <label htmlFor="name">Digite seu CPF:</label>
-                  <input type="text" placeholder="Nome completo" id="name" className="login__input-name" />
-                  <label htmlFor="password">Senha:</label>
-                  <input type="password" id="password" placeholder="********" />
-              </div>
-              <button type="submit">
-                <Link to={"/home"}>Entrar</Link>
-              </button>
-          </form>
-
->>>>>>> 350e1b9793a51bacdedbe87ab17e79fe76c6ecd2
+        {isConfirmed && (
+          <p>Login realizado com sucesso!</p>
+        )}
       </section>
     </div>
   );
